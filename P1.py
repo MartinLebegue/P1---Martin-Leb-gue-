7737f6c1f404dda42d3f9000b926cc54e88d96e3
Martin Lebègue <lebeguecarniato.martin@gmail.com>
2024-03-27 12:39:11 +0100

    Code capable de prendre le titre d'un livre

--- P1.py
+++ P1.py
@@ -1,4 +1,24 @@
-P1 
 
-import miaou 
+import requests 
+from bs4 import BeautifulSoup 
+
+url = "https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
+
+response = requests.get(url)
+
+if response.ok: 
+    soup = BeautifulSoup(response.text)
+    title = soup.find("title")
+    print(title.text)
+
+
+
+
+
+
+
+
+
+
+
 
